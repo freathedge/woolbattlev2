0bdd8dc93809741d4c07d8a31a3c8c13b9943146
freathedge <freathedge@gmail.com>
2025-02-26 20:19:30 +0100

    fixed Blink

--- src/main/java/net/thevace/woolbattle/perks/activeperks/Blink.java
+++ src/main/java/net/thevace/woolbattle/perks/activeperks/Blink.java
@@ -5,10 +5,7 @@ import net.thevace.woolbattle.PerkListenerManager;
 import net.thevace.woolbattle.WoolBattlePlayer;
 import net.thevace.woolbattle.WoolBattlePlayerManager;
 import net.thevace.woolbattle.perks.ActivePerk;
-import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
-import org.bukkit.Location;
-import org.bukkit.Material;
+import org.bukkit.*;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEvent;
@@ -29,8 +26,13 @@ public class Blink extends ActivePerk implements Listener {
         Vector direction = currentLocation.getDirection().normalize().multiply(15);
         Location newLocation = currentLocation.add(direction);
 
+        while (newLocation.getBlock().getType() != Material.AIR || newLocation.clone().add(0, 1, 0).getBlock().getType() != Material.AIR || newLocation.clone().add(0, 2, 0).getBlock().getType() != Material.AIR || newLocation.clone().add(0, 3, 0).getBlock().getType() != Material.AIR) {
+            newLocation.add(0, 1, 0);
+        }
+
         player.getPlayer().teleport(newLocation);
     }
+
 
     @EventHandler
     public void onPlayerInteract(PlayerInteractEvent event) {
